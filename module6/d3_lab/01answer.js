d2d9225a2b2d20dc6a278b337520032b44a888c1
Jered Ataky <70384363+jnataky@users.noreply.github.com>
2021-12-07 21:23:15 -0800

    Update 01answer.js

--- module6/d3_lab/01answer.js
+++ module6/d3_lab/01answer.js
@@ -1,8 +1,28 @@
 d3.csv('ue_industry.csv', data => {
 
     // Define your scales and generator here.
+	
+
+    console.log(data);
+
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.index))
+        .range([40, 1200]);
+
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Agriculture))
+        .range([500, 30]);
+
+    var line = d3.line()
+        .x(d => xScale(+d.index))
+        .y(d => yScale(+d.Agriculture))
+        .curve(d3.curveCardinal);
 
     d3.select('#answer1')
         // append more elements here
+	
+	.append('path')
+        .attr('d', line(data))
+        .attr('stroke', 'black')
 
 });
